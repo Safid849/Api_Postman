3360633072bff0ab551425d24bb1d143f3374ec3
Zinedis <nantenainabakari@gmail.com>
2025-07-11 14:35:44 +0200

    initialization

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from fastapi import FastAPI, requests
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/hello")
-def read_hello(request: Request):
+def read_hello(request: Request, name: str = None, is_teacher: bool = None):
     accept_headers = request.headers.get("Accept")
+
     if accept_headers != "text/plain":
         return JSONResponse({"message": "Unsupported Media Type"}, status_code=400)
-    return JSONResponse(content="Hello world", status_code=200)
+    return JSONResponse(content="Hello World", status_code=200)
 
 
 class WelcomeRequest(BaseModel):
     name: str
 
+
 @app.post("/welcome")
 def welcome_user(request: WelcomeRequest):
-    return {f"Bienvenue {request.name}"}
+    return {"message": f"Bienvenue {request.name}"}
